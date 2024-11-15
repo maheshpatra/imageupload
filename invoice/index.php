0a4fa1d98abe5cb90bddb71be9cd8c7e49facd04
Mahesh <62048436+maheshpatra@users.noreply.github.com>
2024-11-15 18:36:04 +0530

    update

--- invoice/index.php
+++ invoice/index.php
@@ -6,19 +6,19 @@ require_once __DIR__ . '/../validation.php';
 validateApiKey();
 
 // Directory for uploaded invoices
-$invoiceDir = __DIR__ . '/invoice/file/';
+$invoiceDir = __DIR__ . '/uploads/';
 if (!is_dir($invoiceDir)) {
     mkdir($invoiceDir, 0777, true);
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
-    $file = $_FILES['ifile'];
+    $file = $_FILES['file'];
     $fileName = preg_replace('/\s+/', '_', basename($file['name'])); // Sanitize filename
     $fileTmpName = $file['tmp_name'];
     $filePath = $invoiceDir . $fileName;
 
     if (move_uploaded_file($fileTmpName, $filePath)) {
-        $fileUrl = "https://files.finafid.org/invoice/file/" . $fileName;
+        $fileUrl = "https://files.finafid.org/invoice/uploads/" . $fileName;
         echo json_encode(['status' => 'success', 'url' => $fileUrl]);
     } else {
         http_response_code(500);
